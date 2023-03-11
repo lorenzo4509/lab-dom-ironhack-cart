13e67a234affdff0d643cae46c7ed74cb2cc3b79
Lorenzo Hernandez <lorenzorhdez@hotmail.com>
2023-03-11 11:00:12 +0100

    Solved lab

--- src/index.js
+++ src/index.js
@@ -2,49 +2,37 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-  const price = product.querySelector('.price span');
-  const quantity = product.querySelector('.quantity input');
-
-  const priceValue = parseFloat(price.innerHTML);
-  const quantityValue = quantity.valueAsNumber;
-
-  const subtotal = priceValue * quantityValue;
-
-  const subtotalElement = product.querySelector('.subtotal');
-
 
   //... your code goes here
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input');
+  const subtotal = product.querySelector('.subtotal span');
+
+  let priceValue = price.innerHTML;
+  let quantityValue = quantity.value;
+  let subtotalValue = priceValue * quantityValue;
+  subtotal.innerHTML = subtotalValue;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
-  
-    const allProducts = document.getElementsByClassName('product');
-    for (let i = 0; i < allProducts.length; i++) {
-      updateSubtotal(allProducts[i]);
-    }
-  
-  
-
+  const allProducts = document.querySelectorAll('.product');
+  let total = 0;
+  allProducts.forEach((product) => {
+    const subtotal = updateSubtotal(product);
+    total += subtotal;
+  });
+  const totalElement = document.querySelector('#total-value span');
+  totalElement.textContent = total.toFixed(2);
   // ITERATION 3
   //... your code goes here
-  let total = 0;
-  for (let i = 0; i < allProductRows.length; i++) {
-    const productRow = allProductRows[i];
-    const subtotal = updateSubtotal(productRow);
-    total += subtotal;
-  }
-
-  // Update total value on the DOM
-  const totalValueElement = document.getElementById('total-value');
-  totalValueElement.innerHTML = `$${total.toFixed(2)}`;
+  const calculatePricesBtn = document.getElementById('calculate');
+calculatePricesBtn.addEventListener('click', calculateAll);
 }
 
 // ITERATION 4
